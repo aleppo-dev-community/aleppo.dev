0d5848f2da7ae1f9f45b106e009aa1f8f0b30427
Islam Naasani <islamnaasani@gmail.com>
2025-07-13 22:43:47 +0300

    feat: update middleware and auth layout to use redirect search param
    - Modified middleware to include authentication routes in the matcher for improved session management.
    - Enhanced AuthLayout to redirect users based on the 'redirect' query parameter, ensuring a smoother navigation experience after authentication.

--- apps/web/app/auth/layout.tsx
+++ apps/web/app/auth/layout.tsx
@@ -1,11 +1,13 @@
 import { getSession } from "@/lib/get-session";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function AuthLayout({ children }: { children: React.ReactNode }) {
   const session = await getSession();
-
+  const location = (await headers()).get("x-location");
+  const searchParams = new URLSearchParams(location?.split("?")[1]);
   if (session.data) {
-    redirect("/dashboard");
+    redirect(searchParams.get("redirect") ?? "/dashboard");
   }
 
   return children;
