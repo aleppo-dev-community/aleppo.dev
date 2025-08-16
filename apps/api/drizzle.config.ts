852d16423247c3321d7ddabd91b75e95017c6004
Islam Naasani <islamnaasani@gmail.com>
2025-08-16 15:03:01 +0300

    doc: update README with contribution guideline
    - Added a new linting rule for maximum line length in package.json.
    - Enhanced the README with a new contributing section, local development instructions, environment variable setup, and database configuration details.
    - Simplified database connection settings in drizzle.config.ts by using a single URL from environment variables.

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "drizzle-kit";
-const isNeon = process.env.DB_URL?.includes("neon");
-
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/database/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    ssl: isNeon ? "require" : false,
-    database: process.env.DB_NAME!,
-    host: process.env.DB_HOST!,
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
+    url: process.env.DB_URL!,
   },
 });
